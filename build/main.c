7814d9b6543d38af48c210aff00c61bd324b1637
kdonova4 <chicagobears2223@gmail.com>
2024-02-25 16:15:25 -0500

    test change to main.c

--- build/main.c
+++ build/main.c
@@ -155,7 +155,7 @@ void spawn_sand(int mouseX, int mouseY)
 {
 	// Define the range for random sand particle spawning
 	int minSpawn = 5;
-	int maxSpawn = 50;
+	int maxSpawn = 20;
 
 	// Generate a random number of sand particles to spawn
 	int numToSpawn = GetRandomValue(minSpawn, maxSpawn);
